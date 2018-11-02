f82b21e7399a666eb8e0b5a20684ccfa60a192ff
williamganley <william.ganley@missionse.com>
2018-11-01 20:49:21 -0400

    End of first night coding

--- app/src/main/java/com/example/bill/androiduiandlogin_billganley/MainActivity.java
+++ app/src/main/java/com/example/bill/androiduiandlogin_billganley/MainActivity.java
@@ -1,13 +1,54 @@
 package com.example.bill.androiduiandlogin_billganley;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mUserText;
+    private EditText mPasswordText;
+    private Button mButtonLogin;
+    private Button mButtonSignup;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mUserText = findViewById(R.id.editTextUser);
+        mPasswordText = findViewById(R.id.editTextPassword);
+        mButtonLogin = findViewById(R.id.button_login);
+        mButtonSignup = findViewById(R.id.button_signup);
+
+        mButtonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this, LoginSuccessActivity.class);
+                //myIntent.putExtra("key", value); //Optional parameters
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
+
+        mButtonSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this, SignupActivity.class);
+                //myIntent.putExtra("key", value); //Optional parameters
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
     }
 }
