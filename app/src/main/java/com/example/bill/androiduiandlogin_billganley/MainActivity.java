371d7632a25111408a9e5a3b191b10aadc084f5d
williamganley <william.ganley@missionse.com>
2018-11-07 19:43:28 -0500

    Change to Firebase authentication

--- app/src/main/java/com/example/bill/androiduiandlogin_billganley/MainActivity.java
+++ app/src/main/java/com/example/bill/androiduiandlogin_billganley/MainActivity.java
@@ -8,8 +8,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.support.annotation.NonNull;
 
 import java.util.ArrayList;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private Button mButtonSignup;
     private UserProfilePersistence userProfilePersistence;
     ArrayList<UserProfile> userProfiles;
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -31,45 +38,56 @@ public class MainActivity extends AppCompatActivity {
         mPasswordText = findViewById(R.id.editTextPassword);
         mButtonLogin = findViewById(R.id.button_login);
         mButtonSignup = findViewById(R.id.button_signup);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
         mButtonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Need to verify username and password match an existing record in DB
-                userProfilePersistence = new UserProfilePersistence(MainActivity.this);
-                userProfiles = userProfilePersistence.getDataFromDB();
-                Toast.makeText(MainActivity.this,"userProfiles size: "+userProfiles.size(),Toast.LENGTH_SHORT).show();
-                if (userProfiles.size() > 0) {
-                    for (UserProfile up : userProfiles) {
-                        Log.d("MainActivity", "username =  " + up.getUsername());
-                        Log.d("MainActivity", "password =  " + up.getPassword());
-                        Log.d("MainActivity", "entered username =  " + mUserText.getText().toString());
-                        Log.d("MainActivity", "entered password =  " + mPasswordText.getText().toString());
-                        String enteredUser = mUserText.getText().toString();
-                        String enteredPasswored = mPasswordText.getText().toString();
-                        if (enteredUser.equals(up.getUsername().trim())) {
-                            Log.d("MainActivity","username matched");
-                            if (enteredPasswored.equals(up.getPassword())) {
-                                Log.d("MainActivity","password matched");
-                                Intent myIntent = new Intent(MainActivity.this, LoginSuccessActivity.class);
-                                //Add USERNAME to intent for Welcome message
-                                myIntent.putExtra("USERNAME", mUserText.getText().toString());
-                                MainActivity.this.startActivity(myIntent);
-                            }
-                            else {
-                                Log.d("MainActivity","password did not match "+up.getPassword());
-                            }
-                        }
-                        else {
-                            Log.d("MainActivity","username did not match "+up.getUsername());
-                        }
-                    }
-                }
-                else {
-                    Toast.makeText(MainActivity.this,"Press SIGN UP to create account",Toast.LENGTH_LONG).show();
-                }
-            }
+                String enteredUser = mUserText.getText().toString();
+                String enteredPassword = mPasswordText.getText().toString();
+                signIn(enteredUser,enteredPassword);
+           }
         });
+
+//        mButtonLogin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                //Need to verify username and password match an existing record in DB
+//                userProfilePersistence = new UserProfilePersistence(MainActivity.this);
+//                userProfiles = userProfilePersistence.getDataFromDB();
+//                Toast.makeText(MainActivity.this,"userProfiles size: "+userProfiles.size(),Toast.LENGTH_SHORT).show();
+//                if (userProfiles.size() > 0) {
+//                    for (UserProfile up : userProfiles) {
+//                        Log.d("MainActivity", "username =  " + up.getUsername());
+//                        Log.d("MainActivity", "password =  " + up.getPassword());
+//                        Log.d("MainActivity", "entered username =  " + mUserText.getText().toString());
+//                        Log.d("MainActivity", "entered password =  " + mPasswordText.getText().toString());
+//                        String enteredUser = mUserText.getText().toString();
+//                        String enteredPasswored = mPasswordText.getText().toString();
+//                        if (enteredUser.equals(up.getUsername().trim())) {
+//                            Log.d("MainActivity","username matched");
+//                            if (enteredPasswored.equals(up.getPassword())) {
+//                                Log.d("MainActivity","password matched");
+//                                Intent myIntent = new Intent(MainActivity.this, LoginSuccessActivity.class);
+//                                //Add USERNAME to intent for Welcome message
+//                                myIntent.putExtra("USERNAME", mUserText.getText().toString());
+//                                MainActivity.this.startActivity(myIntent);
+//                            }
+//                            else {
+//                                Log.d("MainActivity","password did not match "+up.getPassword());
+//                            }
+//                        }
+//                        else {
+//                            Log.d("MainActivity","username did not match "+up.getUsername());
+//                        }
+//                    }
+//                }
+//                else {
+//                    Toast.makeText(MainActivity.this,"Press SIGN UP to create account",Toast.LENGTH_LONG).show();
+//                }
+//            }
+//        });
 
         mButtonSignup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,5 +97,44 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivity(myIntent);
             }
         });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            Log.d("MainActivity::onStart","User "+currentUser.toString()+" already signed in");
+//            Intent myIntent = new Intent(MainActivity.this,LoginSuccessActivity.class);
+//            myIntent.putExtra("USERNAME", currentUser.getDisplayName());
+//            MainActivity.this.startActivity(myIntent);
+        }
+        else {
+            Log.d("MainActivity::onStart","User NOT signed in");
+        }
+    }
+
+    public void signIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("MainActivity::onCreate", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            String username = user.getEmail();
+                            Intent myIntent = new Intent(MainActivity.this,LoginSuccessActivity.class);
+                            myIntent.putExtra("USERNAME", username);
+                            MainActivity.this.startActivity(myIntent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("MainActivity::onCreate", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed use sign up",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/bill/androiduiandlogin_billganley/SignupActivity.java
+++ app/src/main/java/com/example/bill/androiduiandlogin_billganley/SignupActivity.java
@@ -8,6 +8,12 @@ import android.widget.EditText;
 import android.widget.Button;
 import android.widget.Toast;
 import android.content.Intent;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import android.support.annotation.NonNull;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -21,6 +27,7 @@ public class SignupActivity extends AppCompatActivity {
     private Button mConfirmButton;
     private UserProfilePersistence userProfilePersistence;
     private UserProfile userProfile;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,30 +41,66 @@ public class SignupActivity extends AppCompatActivity {
         mEmailText = (EditText) findViewById(R.id.editTextEmail);
         mPasswordText = (EditText) findViewById(R.id.editTextPassword);
         mConfirmButton = (Button) findViewById(R.id.confirmButton);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
         mConfirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                userProfilePersistence = new UserProfilePersistence(SignupActivity.this);
-
-                userProfile = new UserProfile(  mFirstnameText.getText().toString(),
-                                                mSurnameText.getText().toString(),
-                                                mUserNameText.getText().toString(),
-                                                mBirthdayText.getText().toString(),
-                                                mMobileText.getText().toString(),
-                                                mEmailText.getText().toString(),
-                                                mPasswordText.getText().toString());
-
-                Log.d("SignupActivity", "writing user: "+mFirstnameText.getText().toString()+" "+mSurnameText.getText().toString());
-
-                //Write UserProfile to DB
-                userProfilePersistence.insert(userProfile);
-
-                Toast.makeText(SignupActivity.this,R.string.user_created,Toast.LENGTH_SHORT).show();
-
-                Intent myIntent = new Intent(SignupActivity.this, MainActivity.class);
-                SignupActivity.this.startActivity(myIntent);
+                String email = mEmailText.getText().toString().trim();
+                String password = mPasswordText.getText().toString();
+                Log.d("SignupActivity","email: "+email+" password: "+password);
+                createUser(email,password);
             }
         });
+
+//        mConfirmButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                userProfilePersistence = new UserProfilePersistence(SignupActivity.this);
+//
+//                userProfile = new UserProfile(  mFirstnameText.getText().toString(),
+//                                                mSurnameText.getText().toString(),
+//                                                mUserNameText.getText().toString(),
+//                                                mBirthdayText.getText().toString(),
+//                                                mMobileText.getText().toString(),
+//                                                mEmailText.getText().toString(),
+//                                                mPasswordText.getText().toString());
+//
+//                Log.d("SignupActivity", "writing user: "+mFirstnameText.getText().toString()+" "+mSurnameText.getText().toString());
+//
+//                //Write UserProfile to DB
+//                userProfilePersistence.insert(userProfile);
+//
+//                Toast.makeText(SignupActivity.this,R.string.user_created,Toast.LENGTH_SHORT).show();
+//
+//                Intent myIntent = new Intent(SignupActivity.this, MainActivity.class);
+//                SignupActivity.this.startActivity(myIntent);
+//            }
+//        });
+    }
+
+    public void createUser(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("SignupActivity", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            String username = user.getEmail();
+                            Intent myIntent = new Intent(SignupActivity.this,LoginSuccessActivity.class);
+                            myIntent.putExtra("USERNAME", username);
+                            SignupActivity.this.startActivity(myIntent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("SignupActivity", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignupActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
     }
 }
